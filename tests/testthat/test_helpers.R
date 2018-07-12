5df99b3ce4ab7353ed187b8e78650f9db2d5ee26
Jiew-Quay Au <auj@STAT.UNI-MUENCHEN.DE>
2018-07-12 13:26:30 +0200

    add unit tests for helpers

---
+++ tests/testthat/test_helpers.R
@@ -0,0 +1,40 @@
+context("helpers")
+
+test_that("add time variables", {
+  toydata = data.frame(timestamp = c(1531393277, 915152461,-631144492))
+  toydata2 = toydata
+  toydata2$timestamp = toydata2$timestamp * 1000
+  #1531393277 is 07/12/2018 @ 11:01:17am (UTC) Thu, 12 Jul 2018
+  #915152461 is 01/01/1999 @ 1:01:01am (UTC) Fri, 01 Jan
+  #-631144492 01/01/1950 @ 2:05:08am (UTC) Sun, 01 Jan
+
+  #check addWeekday
+  x = addWeekday(toydata)
+  assertFactor(x$weekday)
+  assertTRUE(all.equal(as.character(x$weekday), c("Thu", "Fri", "Sun")))
+
+  x2 = addWeekday(toydata2, unit = "ms")
+  assertFactor(x2$weekday)
+  assertTRUE(all.equal(as.character(x2$weekday), c("Thu", "Fri", "Sun")))
+
+
+  #check addTime
+  x = addTime(toydata)
+  assertCharacter(x$time)
+  assertTRUE(all.equal(as.character(x$time), c("11:01:17", "01:01:01", "02:05:08")))
+
+  x2 = addTime(toydata2, unit = "ms")
+  assertCharacter(x2$time)
+  assertTRUE(all.equal(as.character(x2$time), c("11:01:17", "01:01:01", "02:05:08")))
+
+
+  #check addDateTime
+  x = addDateTime(toydata)
+  assertTRUE(all.equal(as.character(x$date_time),
+    c("2018-07-12 11:01:17", "1999-01-01 01:01:01", "1950-01-01 02:05:08")))
+
+  x2 = addDateTime(toydata2, unit = "ms")
+  assertTRUE(all.equal(as.character(x2$date_time),
+    c("2018-07-12 11:01:17", "1999-01-01 01:01:01", "1950-01-01 02:05:08")))
+
+})
