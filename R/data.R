f997b83e9c1ca449c986620450f63f889312af79
QuayAu <quayau@gmail.com>
2018-11-29 15:04:15 +0100

    update studentlife.small

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@
 
 #' studentlife.small
 #'
-#' Contains exemplary gps and app data from the studentlife dataset.
+#' Contains exemplary gps and app data from the studentlife dataset (users: u00, u01, u02)
 #'
 #' http://studentlife.cs.dartmouth.edu/dataset.html
 #'
@@ -18,3 +18,31 @@
 #' @keywords data
 #' @docType data
 "studentlife.small"
+
+# library(dplyr)
+# gpsdummy = read.csv("data_gitignore/studentlife/dataset/sensing/gps/gps_u00.csv")
+# cn = c(names(gpsdummy), "DELETEME")
+# gps00 = read.csv("data_gitignore/studentlife/dataset/sensing/gps/gps_u00.csv", col.names = cn) %>%
+#   select(-DELETEME) %>%
+#   addColumn(function(x) rep("00", nrow(x)), colname = "userId") %>%
+#   mutate(timestamp = time, time = NULL)
+# gps01 = read.csv("data_gitignore/studentlife/dataset/sensing/gps/gps_u01.csv", col.names = cn) %>%
+#   select(-DELETEME) %>%
+#   addColumn(function(x) rep("01", nrow(x)), colname = "userId") %>%
+#   mutate(timestamp = time, time = NULL)
+# gps02 = read.csv("data_gitignore/studentlife/dataset/sensing/gps/gps_u02.csv", col.names = cn) %>%
+#   select(-DELETEME) %>%
+#   addColumn(function(x) rep("02", nrow(x)), colname = "userId") %>%
+#   mutate(timestamp = time, time = NULL)
+# app00 = read.csv("data_gitignore/studentlife/dataset/app_usage/running_app_u00.csv") %>% addColumn(function(x) rep("00", nrow(x)), colname = "userId")
+# app01 = read.csv("data_gitignore/studentlife/dataset/app_usage/running_app_u01.csv") %>% addColumn(function(x) rep("01", nrow(x)), colname = "userId")
+# app02 = read.csv("data_gitignore/studentlife/dataset/app_usage/running_app_u02.csv") %>% addColumn(function(x) rep("02", nrow(x)), colname = "userId")
+#
+#
+# d00 = gps00 %>% bind_rows(app00) %>% arrange(timestamp)
+# d01 = gps01 %>% bind_rows(app01) %>% arrange(timestamp)
+# d02 = gps02 %>% bind_rows(app02) %>% arrange(timestamp)
+# studentlife.small = d00 %>% bind_rows(d01) %>% bind_rows(d02)
+# studentlife.small$ï..id = NULL
+# library(devtools)
+# use_data(studentlife.small, overwrite = TRUE)
