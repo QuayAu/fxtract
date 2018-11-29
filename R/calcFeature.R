8ea85164e6c1361936444f9c442a85f7a5f9c6bf
QuayAu <quayau@gmail.com>
2018-11-29 16:18:21 +0100

    add summarize function for calcFeature

--- R/calcFeature.R
+++ R/calcFeature.R
@@ -4,34 +4,46 @@
 #' @template param_group_col
 #' @template param_fun_1
 #' @template param_check_fun
+#' @param summarize function. If more than one value is returned for each group, this function summarizes these values to one value. E.g. mean.
+#' @param colname character. If one variable is returned for each group, you can specify a custom column name for this new column.
 #' @family feature functions
 #' @return dataframe
-#' @importFrom dplyr group_by do
+#' @importFrom dplyr group_by do one_of
 #' @importFrom magrittr "%>%"
 #' @importFrom data.table dcast setDT
-#' @importFrom stats as.formula
+#' @importFrom stats as.formula setNames
 #' @export
 #' @examples
 #' number_uses_chrome = function(data) {
 #'   nrow(dplyr::filter(data, RUNNING_TASKS_baseActivity_mPackage == "com.android.chrome"))
 #' }
 #' calcFeature(data = studentlife.small, group_col = "userId", fun = number_uses_chrome)
-calcFeature = function(data, group_col, fun, check_fun = TRUE) {
+calcFeature = function(data, group_col, fun, check_fun = TRUE, summarize, colname) {
   . = NULL
   checkmate::assertDataFrame(data)
   checkmate::assertNames(names(data), must.include = group_col)
   checkmate::assertLogical(check_fun)
-
   if (check_fun) {
     if (dim(do.call(fun, list(data)) %>% data.frame())[1] != 1) stop("fun must return a vector of length 1 or a dataframe with 1 row!")
   }
-  
+
+  if (!missing(summarize)) checkmate::assertFunction(summarize)
+  if (!missing(colname)) checkmate::assertCharacter(colname)
+
   if (length(group_col) == 1) res = data %>% dplyr::group_by_(.dots = group_col) %>% dplyr::do(data.frame(do.call(fun, list(.))))
   if (length(group_col) >= 2) {
     res = data %>% dplyr::group_by_(.dots = group_col) %>% dplyr::do(data.frame(do.call(fun, list(.))))
     dcast_formula = as.formula(paste(group_col[1], "~", paste(group_col[-1], collapse = "+")))
-    res = data.table::dcast(data.table::setDT(res), dcast_formula, value.var = setdiff(colnames(res), group_col)) 
+    res = data.table::dcast(data.table::setDT(res), dcast_formula, value.var = setdiff(colnames(res), group_col))
   }
-  if (ncol(res) == 2) colnames(res)[2] = deparse(substitute(fun))
-  return(data.frame(res))
+  if (ncol(res) == 2) colnames(res)[2] = ifelse(missing(colname), deparse(substitute(fun)), colname)
+  res = data.frame(res)
+
+  if (!missing(summarize)) {
+    res0 = res %>% select(-one_of(group_col[1]))
+    res0 = data.frame(apply(res0, 1, function(x) do.call(summarize, list(x))))
+    if (!missing(colname)) res0 = res0 %>% setNames(colname)
+    res = data.frame(res %>% select(one_of(group_col[1])), res0)
+  }
+  return(res)
 }
--- tests/testthat/test_feature_functions.R
+++ tests/testthat/test_feature_functions.R
@@ -5,7 +5,7 @@ test_that("calcFeature", {
   df$task = rep(c(rep("task1", 5), rep("task2", 5)), 2)
   df$hour = rep(c(rep("hour1", 3), rep("hour2", 2), rep("hour1", 2), rep("hour2", 3)), 2)
   df$x = 1:20
-  
+
   #test checks
   fun = function(data) data$x
   expect_error(calcFeature(data = df, group_col = "id", fun = fun),
@@ -15,7 +15,7 @@ test_that("calcFeature", {
   mean_x = function(data) mean(data$x)
   y = calcFeature(data = df, group_col = "id", fun = mean_x)
   expect_equal(y$mean_x, c(mean(1:10), mean(11:20)))
-  
+
   #return 1 row of data
   more_features = function(data) data.frame(mean_x = mean(data$x), sd_x = sd(data$x), min_x = min(data$x))
   y = calcFeature(data = df, group_col = "id", fun = more_features)
@@ -26,14 +26,24 @@ test_that("calcFeature", {
   #group by 2 columns
   y = calcFeature(data = df, group_col = c("id", "task"), fun = more_features)
   expect_equal(dim(y), c(2, 7))
-  
+
   #group by 3 columns
   y = calcFeature(data = df, group_col = c("id", "task", "hour"), fun = more_features)
   expect_equal(dim(y), c(2, 3 * 4 + 1))
-  
+
   #group by 4 columns
   df$y = c(rep(1, 2), rep(2, 2))
   y = calcFeature(data = df, group_col = c("id", "task", "hour", "y"), fun = more_features)
   expect_equal(dim(y), c(2, 3 * 4 * 2 + 1))
+
+  #change colname
+  mean_x = function(data) mean(data$x)
+  y = calcFeature(data = df, group_col = "id", fun = mean_x, colname = "mean_of_x")
+  expect_equal(colnames(y)[2], "mean_of_x")
+
+  #summarize function
+  y1 = calcFeature(data = df, group_col = c("id", "hour"), fun = mean_x)
+  y2 = calcFeature(data = df, group_col = c("id", "hour"), fun = mean_x, summarize = mean)
+  expect_equal(y2[, 2], rowMeans(y1[, -1]))
 })
 
