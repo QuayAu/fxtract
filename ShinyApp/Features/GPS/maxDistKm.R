e89bf7e0be5dec84d928a4662679a8322b74342b
andreasho95 <andreas.hofheinz@outlook.com>
2018-11-30 16:45:16 +0100

    Prevent error when no gps data available

--- ShinyApp/Features/GPS/maxDistKm.R
+++ ShinyApp/Features/GPS/maxDistKm.R
@@ -1,6 +1,6 @@
 maxDistKm = function(df) {
   gpsAvailable = df %>% filter(!is.na(latitude), !is.na(longitude), latitude >= 0, latitude <= 360, longitude >= 0, longitude <= 360)
-  # Add check wether gps are available -> if not enter NA with warning message
+  if (nrow(gpsAvailable) == 0) return(data.frame("max" = NA))
   x = max(distm(cbind(gpsAvailable$longitude,gpsAvailable$latitude))) / 1000
   data.frame("max" = x)
 }
